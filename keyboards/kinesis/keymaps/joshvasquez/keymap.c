ed24853f679123a83b7852c22bd00b44f89b046c
Joshua Vasquez <least@daisuke.dev>
2021-05-31 03:30:39 +0000

    change binds kinesis

--- keyboards/kinesis/keymaps/joshvasquez/keymap.c
+++ keyboards/kinesis/keymaps/joshvasquez/keymap.c
@@ -49,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_CAPS, KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,
            KC_EQL, KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,
            MT_TAB, KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
-           MT_ESC,CTL_T(KC_A),LGUI_T(KC_S),LALT_T(KC_D),SFT_T(KC_F) ,KC_G   ,
-           KC_LSFT,CST_Z  ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,
+           MT_ESC,C_S_T(KC_A),KC_S,KC_D,KC_F ,KC_G   ,
+           KC_LSFT,SFT_T(KC_Z)  ,LGUI_T(KC_X)   ,LALT_T(KC_C)   ,KC_V   ,KC_B   ,
                    KC_GRV ,KC_TILD,KC_LEFT,KC_RGHT,
 			   KC_LGUI,KC_LALT,
                                     KC_HOME,
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F9,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR   ,KC_VOLU  ,KC_VOLD, TG(GAMING), RESET,
 	KC_6 ,KC_7   ,KC_8   ,KC_9   ,KC_0      ,KC_MINS,
 	KC_Y ,KC_U   ,KC_I   ,KC_O   ,KC_P      ,KC_BSLS,
-	KC_H ,SFT_T(KC_J),LALT_T(KC_K),LGUI_T(KC_L),CTL_T(KC_SCLN),MT_QUOT,
-	KC_N ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH   ,KC_RSFT,
+	KC_H ,KC_J,KC_K,KC_L,KC_SCLN,MT_QUOT,
+	KC_N ,KC_M   ,LALT_T(KC_COMM),LGUI_T(KC_DOT) ,SFT_T(KC_SLSH)   ,KC_RSFT,
 		KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,
            KC_RGUI,KC_RCTL,
            KC_PGUP,
@@ -217,21 +217,12 @@ void matrix_init_user(void) {
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
     case QWERTY:
-        ledTurnOff(LED_KEYPAD);
-        ledTurnOff(LED_SCROLL_LOCK);
-        ledTurnOff(LED_CAPS_LOCK);
-        ledTurnOff(LED_NUM_LOCK);
         break;
     case GAMING:
-        ledTurnOff(LED_SCROLL_LOCK);
-        ledTurnOff(LED_CAPS_LOCK);
-        ledTurnOff(LED_NUM_LOCK);
-        ledTurnOn(LED_KEYPAD);
         break;
     case SYMBOL:
         break;
     case MMEDIA:
-        ledTurnOn(LED_CAPS_LOCK);
             break;
     default: //  for any other layers, or the default layer
         break;
